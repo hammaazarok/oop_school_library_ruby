f6ec9bcf8f0cf4ff3c31b8d698ee4da0b636b26e
Hamma Azarok <hammaagazarok@gmail.com>
2022-09-26 17:07:54 +0200

    fix Linters

--- person.rb
+++ person.rb
@@ -1,22 +1,20 @@
 class Person
+  attr_reader :id, :name, :age
 
-    attr_reader :id
-    attr_reader :name
-    attr_reader :age
-    def initialize (age , name = 'Unknown' , parent_permission = true)
-        @id = Random.rand(0..1000)
-        @name = name
-        @age = age
-        @parent_permission
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(0..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def is_of_age?
-        return @age > 17
-    end
+  def of_age?
+    @age > 17
+  end
 
-    def can_use_services?
-        return is_of_age? || parent_permission
-    end
-    
-    private :is_of_age?
+  def can_use_services?
+    of_age? || parent_permission
+  end
+
+  private :is_of_age?
 end
