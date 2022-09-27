496ed39fde07fa2dc7072ed1833af3f2218f7d92
Hamma Azarok <hammaagazarok@gmail.com>
2022-09-27 16:52:37 +0200

    add main ruby file to run decorators using test cases provided

---
+++ main.rb
@@ -0,0 +1,10 @@
+require './person.rb'
+require './capitalize_decorator.rb'
+require './trimmer_decorator.rb'
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
