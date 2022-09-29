974aa162c7134763c3b7f00b8d387098187ed215
Hamma Azarok <hammaagazarok@gmail.com>
2022-09-29 13:11:56 +0200

    add rental and book with thier relationships

--- student.rb
+++ student.rb
@@ -4,6 +4,11 @@ class Student < Person
     super(age, name, parent_permission)
     @classroom = classroom
   end
+  
+  def class(clssroom)
+    @classroom = classroom
+    clssroom.students_list.push(self) unless clssroom.students_list.include?(self)
+  end
 
   def play_hooky
     "¯\(ツ)/¯"
